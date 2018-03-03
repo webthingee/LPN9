ded670459e7ada2e06fcd25ee83e0d9a71a340db
Sean Lange <sean@webthingee.com>
2018-03-02 22:14:11 -0800

    fix menu structure, Astar needs to be in the root 2

---
+++ Assets/Rooms/RoomContents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomContents : MonoBehaviour 
+{
+    public Transform[] enemySpots;
+    public Transform[] rewardSpots;
+    public Transform[] lightSpots;
+
+}
---
+++ Assets/Rooms/RoomPrefabs.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomPrefabs : MonoBehaviour 
+{
+	public List<GameObject> Walkways = new List<GameObject>();
+    public List<GameObject> Landings = new List<GameObject>();
+    public List<GameObject> Drops = new List<GameObject>();
+    public List<GameObject> Universal = new List<GameObject>();
+}
