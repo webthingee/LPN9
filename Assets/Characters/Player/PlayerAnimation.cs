5020f0fc264bb43a118828bd093271ec6bca5aa2
Sean Lange <sean@webthingee.com>
2018-03-04 09:58:00 -0800

    new tiles, art, animations

--- Assets/Characters/Player/PlayerAnimation.cs
+++ Assets/Characters/Player/PlayerAnimation.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour 
 {
-    public SpriteRenderer characterSprite;
-
-    private CharacterMovement cm;
+    public GameObject playerSprite;
+    
+    SpriteRenderer characterSprite;
+    Animator anim;
+    CharacterMovement cm;
         
     void Awake ()
     {
         cm = GetComponent<CharacterMovement>();
+        characterSprite = playerSprite.GetComponent<SpriteRenderer>();
+        anim = playerSprite.GetComponent<Animator>();
     }
 
     void Update ()
     {        
         ChangeDirection(cm.GetMoveDirection.x);
+
+        anim.SetFloat("Forward", Mathf.Abs(cm.GetMoveDirection.x));
     }
 
     void ChangeDirection (float _direction)
