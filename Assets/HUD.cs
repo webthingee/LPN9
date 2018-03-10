eb7d92382f205cfac85bfe0999768c86a41b2ab5
Sean Lange <sean@webthingee.com>
2018-03-09 20:45:41 -0800

    hud1

---
+++ Assets/HUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour 
+{
+    public Text healthTXT;
+    public Text obolsTXT;
+
+    void Start()
+    {
+    }
+
+    void Update ()
+    {
+        healthTXT.text = "All Time Levels Played = " + ManagePrefs.MP.GamesPlayed.ToString();
+        obolsTXT.text = "All Time Coins Collected = " + ManagePrefs.MP.Obols.ToString();
+    } 
+}
